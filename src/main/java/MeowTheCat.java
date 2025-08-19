65f0239e6aec591fa1a575c4693ee26bccf48f0b
RandomNerd01 <suiron8@gmail.com>
2025-08-19 10:09:12 +0800

    Level-1: Echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/MeowTheCat.java
+++ src/main/java/MeowTheCat.java
@@ -1,10 +1,28 @@
-public class MeowTheCat {
+import java.util.Scanner;
+
+public class MeowTheCatLevel1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm MeowTheCat");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String line = sc.nextLine().trim();
+            if (line.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(line);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        sc.close();
     }
 }
