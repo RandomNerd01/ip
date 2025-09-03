c42c03409f15fcce943db024dafa8e193ce2b9bd
RandomNerd01 <suiron8@gmail.com>
2025-09-03 16:09:41 +0800

    Implement GUI

---
+++ src/main/java/meowthecat/Main.java
@@ -0,0 +1,31 @@
+package meowthecat;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for MeowTheCat using FXML.
+ */
+public class Main extends Application {
+
+    private MeowCat meow = new MeowCat();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setMeow(meow);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("MainWindow.fxml");
+        }
+    }
+}
