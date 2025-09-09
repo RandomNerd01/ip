a29a52dc50235440e000c74cb8e6181be9fd3cf0
RandomNerd01 <suiron8@gmail.com>
2025-09-09 15:52:13 +0800

    Improve code style and clarity
    Current situation: A lot of the code does not follow
    java coding standard
    
    Why change: Improve developer experience by making
    code more readable
    
    What is done:
    Add Javadoc to public methods where missing.
    Follow java coding standard.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# MeowTheCat User Guide
+# Duke User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/meowthecat/Launcher.java
+++ src/main/java/meowthecat/Launcher.java
@@ -1,7 +1,6 @@
 package meowthecat;
 import javafx.application.Application;
 
-
 /**
  * A launcher class to workaround classpath issues.
  */
